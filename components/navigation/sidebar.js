593d6790594ea0c439ef54504b6a4bd266ee7952
Anthony Hadwan <anthony.hadwan@hec.edu>
2023-10-21 13:24:57 +0200

    first commit and initial setup

---
+++ components/navigation/sidebar.js
@@ -0,0 +1,13 @@
+const Sidebar = () => {
+  return (
+    <div className=" w-40 border-r">
+      <div>Image is here</div>
+      <h1>Aggregatoor</h1>
+      <ul>
+        <li>All News</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Sidebar;
