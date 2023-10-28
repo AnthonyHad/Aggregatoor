a6c3eaeee39ff4d33ca751388ff6d8ec476e4852
Anthony Hadwan <anthony.hadwan@hec.edu>
2023-10-28 12:49:35 +0200

    fixed footer and added further responsiveness to sidebar

--- components/navigation/footer.js
+++ components/navigation/footer.js
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <div className="py-8 border-t mt-10">
-      <div className="px-8 w-full mx-auto max-w-7xl">
+      <div className="px-6 w-full mx-auto max-w-5xl">
         <h2 className="text-3xl font-bold">Aggregatooor</h2>
         <p className="max-w-md mt-2 text-neutral-700 text-lg">
           Blockchain news and more!
--- components/navigation/sidebar.js
+++ components/navigation/sidebar.js
@@ -1,20 +1,79 @@
-import { Newspaper } from "lucide-react";
+"use client";
+import { useState, useEffect } from "react";
+import { Newspaper, MenuSquare, X } from "lucide-react";
 
 const Sidebar = () => {
+  const [isSidebarVisible, setIsSidebarVisible] = useState(true);
+
+  useEffect(() => {
+    function handleResize() {
+      if (window.innerWidth <= 600) {
+        setIsSidebarVisible(false);
+      } else {
+        setIsSidebarVisible(true);
+      }
+    }
+
+    window.addEventListener("resize", handleResize);
+
+    handleResize();
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  const toggleSidebar = () => {
+    setIsSidebarVisible(!isSidebarVisible);
+  };
+
   return (
-    <div className=" w-40 border-r flex flex-col items-center">
-      <div className="sticky top-0 space-y-4 flex flex-col items-center">
-        <div className="mt-8">Image is here</div>
-        <h1 className="text-xl font-semibold">Aggregatoor</h1>
-        <ul>
-          <li className="flex items-center gap-1">
-            <Newspaper size={16} />
-            All News
-          </li>
-        </ul>
+    <>
+      <div
+        className={`w-40 border-r ${
+          isSidebarVisible ? "" : "hidden"
+        } flex flex-col items-center`}
+      >
+        {window.innerWidth <= 600 && (
+          <button onClick={toggleSidebar} className="mb-2">
+            <X />
+          </button>
+        )}
+
+        <div className="sticky top-0 space-y-4 flex flex-col items-center">
+          <div className="mt-8">Image is here</div>
+          <h1 className="text-xl font-semibold">Aggregatoor</h1>
+          <ul>
+            <li className="flex items-center gap-1">
+              <Newspaper size={16} />
+              All News
+            </li>
+          </ul>
+        </div>
       </div>
-    </div>
+      <div>
+        {!isSidebarVisible && (
+          <button onClick={toggleSidebar}>
+            <MenuSquare />
+          </button>
+        )}
+      </div>
+    </>
   );
 };
 
 export default Sidebar;
+
+// {
+//   !isSidebarVisible ? (
+//     <button onClick={toggleSidebar}>
+//       <MenuSquare />
+//     </button>
+//   ) : window.innerWidth <= 600 ? (
+//     <button onClick={toggleSidebar}>
+//       <MenuSquare />
+//     </button>
+//   ) : (
+//     ""
+//   );
+// }
