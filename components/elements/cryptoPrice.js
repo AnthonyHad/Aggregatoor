d036bc9a73d12747aa5b8625c23ddd86da9cf737
Anthony Hadwan <anthony.hadwan@hec.edu>
2023-10-29 21:35:23 +0100

    some change to price UI

--- components/elements/cryptoPrice.js
+++ components/elements/cryptoPrice.js
@@ -1,19 +1,27 @@
 import Image from "next/image";
 import { ChevronDown, ChevronUp } from "lucide-react";
 
-const CryptoPrice = ({ key, ticker, currentPrice, image, percentChange }) => {
+const CryptoPrice = ({ ticker, currentPrice, image, percentChange }) => {
+  const formattedTicker = ticker.toUpperCase();
+  const formattedPercentChange = `${percentChange.toFixed(2)}%`;
+  const formattedPrice = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 0,
+  }).format(currentPrice);
+
   return (
-    <div key={key} className="flex space-x-1.5 items-center">
-      <Image src={image} width={15} height={10} alt={ticker} />
-      <div>{ticker}</div>
-      <p>{currentPrice}</p>
+    <div className="flex space-x-1 items-center p-2">
+      <Image src={image} width={20} height={20} alt={ticker} />
+      <div className="font-bold">{formattedTicker}</div>
+      <p className="text-sm">{formattedPrice}</p>
       <div className="text-sm flex items-center">
         {percentChange >= 0 ? (
-          <ChevronUp size={12} />
+          <ChevronUp size={12} color="#14532d" />
         ) : (
-          <ChevronDown size={12} />
+          <ChevronDown size={12} color="#7f1d1d" />
         )}
-        {percentChange}
+        {formattedPercentChange}
       </div>
     </div>
   );
