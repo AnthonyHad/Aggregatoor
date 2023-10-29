5ed83e1a75126919ca5cec5946ace7a14c515db3
Anthony Hadwan <anthony.hadwan@hec.edu>
2023-10-29 19:41:10 +0100

    small modifications

--- components/navigation/footer.js
+++ components/navigation/footer.js
@@ -1,9 +1,9 @@
 const Footer = () => {
   return (
     <div className="py-8 border-t mt-10">
-      <div className="px-6 w-full mx-auto max-w-5xl">
+      <div className="px-6 w-full mx-auto">
         <h2 className="text-3xl font-bold">Aggregatooor</h2>
-        <p className="max-w-md mt-2 text-neutral-700 text-lg">
+        <p className="mt-2 text-neutral-700 text-lg">
           Blockchain news and more!
         </p>
       </div>
