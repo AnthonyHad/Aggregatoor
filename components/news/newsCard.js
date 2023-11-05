f0526d70f6a8199a44ace3e08af92bcccf111e9a
Anthony Hadwan <anthony.hadwan@hec.edu>
2023-11-05 12:51:13 +0100

    added links to source on each article

--- components/news/newsCard.js
+++ components/news/newsCard.js
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 const NewsCard = ({ title, link, pubDate, contentSnippet, source, logo }) => {
   const currentTime = new Date().getTime();
@@ -19,24 +20,26 @@ const NewsCard = ({ title, link, pubDate, contentSnippet, source, logo }) => {
   }
 
   return (
-    <div className=" rounded-md  w-full h-[270px] bg-gradient-to-br from-[#4B79A1] to-[#283E51] transition ease-in-out delay-50  hover:-translate-y-2">
-      <div className="flex flex-col gap-3 justify-end h-full p-4 ">
-        <div className="flex items-center gap-2">
-          {logo && (
-            <div>
-              <Image src={logo} width={120} height={120} alt={source} />
-            </div>
-          )}
-          <div className="w-1  h-1 rounded-full bg-neutral-100" />
-          <p className="text-xs font-thin italic">{timeAgoText}</p>
-        </div>
+    <Link href={link} target="_blank">
+      <div className=" rounded-md  w-full h-[270px] bg-gradient-to-br from-[#4B79A1] to-[#283E51] transition ease-in-out delay-50  hover:-translate-y-2">
+        <div className="flex flex-col gap-3 justify-end h-full p-4 ">
+          <div className="flex items-center gap-2">
+            {logo && (
+              <div>
+                <Image src={logo} width={120} height={120} alt={source} />
+              </div>
+            )}
+            <div className="w-1  h-1 rounded-full bg-neutral-100" />
+            <p className="text-xs font-thin italic">{timeAgoText}</p>
+          </div>
 
-        <h3 className="font-bold">{title}</h3>
-        <p className="line-clamp-3 text-ellipsis text-sm font-light">
-          {contentSnippet}
-        </p>
+          <h3 className="font-bold">{title}</h3>
+          <p className="line-clamp-3 text-ellipsis text-sm font-light">
+            {contentSnippet}
+          </p>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
